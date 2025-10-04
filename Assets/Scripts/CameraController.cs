fdcc02be0040ba940045453b789e7b50e3f3b99d
gustavoTCXR <143439516+amoam-vr@users.noreply.github.com>
2025-10-03 21:30:07 -0700

    completed laser set and camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float followSpeed;
+    public float zDistance;
+    private Vector2 currentDistance;
+
+    private void Update()
+    {
+        currentDistance = Vector3.Lerp(transform.position, Toy.possessedToy.transform.position, 0.01f * followSpeed);
+        transform.position = new Vector3(currentDistance.x, currentDistance.y, Toy.possessedToy.transform.position.z - zDistance);
+    }
+}
