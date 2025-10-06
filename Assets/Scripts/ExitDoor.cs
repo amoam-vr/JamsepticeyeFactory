4eecd241a064f092dccb6b9255b12d6b756103c5
gustavoTCXR <143439516+amoam-vr@users.noreply.github.com>
2025-10-05 19:56:45 -0700

    fixed tutorial exiting to tutorial

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -6,7 +6,7 @@ public class ExitDoor : MonoBehaviour
     // Author: Gustavo
     // Loads the next scene on contact
     
-    static int currentLevel = 0;
+    static int currentLevel = 1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
